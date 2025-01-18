3894aba0645fe6f96a035c7b38089d317d048a2c
Hun Kim <hunkim98@gmail.com>
2025-01-17 23:55:31 -0500

    Create logic for pasting image as user navigates

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -43,7 +43,56 @@ public class MapGenerator : MonoBehaviour
         // In this we need to see if a new tile should be placed
         // Get user position
         Vector3 userPosition = Camera.main.transform.position;
+        CheckAdditionalMapTile(new Vector2(userPosition.x, userPosition.y));
         
+    }
+
+    void CheckAdditionalMapTile(Vector2 userPosition)
+    {
+        // Check if we need to add a new tile
+        // Get the user position in the map
+        Vector2 topPos = new Vector2(userPosition.x, userPosition.y + spriteSquareSize / 2);
+        Vector2 bottomPos = new Vector2(userPosition.x, userPosition.y - spriteSquareSize / 2);
+        Vector2 leftPos = new Vector2(userPosition.x - spriteSquareSize / 2, userPosition.y);
+        Vector2 rightPos = new Vector2(userPosition.x + spriteSquareSize / 2, userPosition.y);
+
+        Vector2 topLeftPos = new Vector2(userPosition.x - spriteSquareSize / 2, userPosition.y + (float)(spriteSquareSize/Math.Sqrt(2)));
+        Vector2 topRightPos = new Vector2(userPosition.x + spriteSquareSize / 2, userPosition.y + (float)(spriteSquareSize/Math.Sqrt(2)));
+        Vector2 bottomLeftPos = new Vector2(userPosition.x - spriteSquareSize / 2, userPosition.y - (float)(spriteSquareSize/Math.Sqrt(2)));
+        Vector2 bottomRightPos = new Vector2(userPosition.x + spriteSquareSize / 2, userPosition.y - (float)(spriteSquareSize/Math.Sqrt(2)));
+
+        Vector2 topPosInt = new Vector2((int) Math.Round(topPos.x / (spriteSquareSize/2)), (int) Math.Round(topPos.y / (spriteSquareSize/2)));
+        Vector2 bottomPosInt = new Vector2((int) Math.Round(bottomPos.x / (spriteSquareSize/2)), (int) Math.Round(bottomPos.y / (spriteSquareSize/2)));
+        Vector2 leftPosInt = new Vector2((int) Math.Round(leftPos.x / (spriteSquareSize/2)), (int) Math.Round(leftPos.y / (spriteSquareSize/2)));
+        Vector2 rightPosInt = new Vector2((int) Math.Round(rightPos.x / (spriteSquareSize/2)), (int) Math.Round(rightPos.y / (spriteSquareSize/2)));
+
+        Vector2 topLeftPosInt = new Vector2((int) Math.Round(topLeftPos.x / (spriteSquareSize/2)), (int) Math.Round(topLeftPos.y / (spriteSquareSize/2)));
+        Vector2 topRightPosInt = new Vector2((int) Math.Round(topRightPos.x / (spriteSquareSize/2)), (int) Math.Round(topRightPos.y / (spriteSquareSize/2)));
+        Vector2 bottomLeftPosInt = new Vector2((int) Math.Round(bottomLeftPos.x / (spriteSquareSize/2)), (int) Math.Round(bottomLeftPos.y / (spriteSquareSize/2)));
+        Vector2 bottomRightPosInt = new Vector2((int) Math.Round(bottomRightPos.x / (spriteSquareSize/2)), (int) Math.Round(bottomRightPos.y / (spriteSquareSize/2)));
+
+        Debug.Log("Top: " + topPosInt.x + "-" + topPosInt.y);
+        // Debug.Log("Right: " + rightPosInt.x + "-" + rightPosInt.y);
+        // Debug.Log("Bottom: " + bottomPosInt.x + "-" + bottomPosInt.y);
+        // Debug.Log("Left: " + leftPosInt.x + "-" + leftPosInt.y);
+
+        // Debug.Log("TopLeft: " + topLeftPosInt.x + "-" + topLeftPosInt.y);
+        // Debug.Log("TopRight: " + topRightPosInt.x + "-" + topRightPosInt.y); 
+        // Debug.Log("BottomLeft: " + bottomLeftPosInt.x + "-" + bottomLeftPosInt.y);
+        // Debug.Log("BottomRight: " + bottomRightPosInt.x + "-" + bottomRightPosInt.y);
+
+        Vector2[] checkPositions = new Vector2[] {topPosInt, bottomPosInt, leftPosInt, rightPosInt, topLeftPosInt, topRightPosInt, bottomLeftPosInt, bottomRightPosInt};
+        for (int i = 0; i < checkPositions.Length; i++)
+        {
+            // check if the key is contianed
+            string checkPositionTileId = TileTools.GenerateId((int)checkPositions[i].x, (int)checkPositions[i].y);
+            if (!spriteRenderStatusDict.ContainsKey(checkPositionTileId))
+            {
+                // Generate the tile
+                GenerateAdditionalMapTile((int)checkPositions[i].x, (int)checkPositions[i].y);
+            }
+        }
+
     }
 
     void Initialize() 
@@ -99,7 +148,7 @@ public class MapGenerator : MonoBehaviour
             float scale = spriteSquareSize / spriteSize.x;
             spriteGameObject.transform.localScale = new Vector3(scale, scale, 1);
             // position the game object
-            spriteGameObject.transform.position = new Vector3(tileIndex.x * spriteSquareSize, tileIndex.y * spriteSquareSize, 0);
+            spriteGameObject.transform.position = new Vector3(tileIndex.x * (spriteSquareSize/2), tileIndex.y * (spriteSquareSize/2), 0);
             // add the game object to the map container
             spriteGameObject.transform.parent = mapContainer.transform;
             Debug.Log("Image loaded successfully");
@@ -125,8 +174,12 @@ public class MapGenerator : MonoBehaviour
         GameObject spriteObject = CreateSpriteGameObject(imagePath, new Vector2(0, 0));
     }
 
-    void GenerateAdditionalMapTile(int positionX, int positionY)
+    GameObject GenerateAdditionalMapTile(int indexX, int indexY, string imagePath = "Assets/Images/example.jpeg")
     {
-
+        string tileId = TileTools.GenerateId(indexX, indexY);
+        spriteRenderStatusDict.Add(tileId, SpriteRenderStatus.Pending);
+        GameObject spriteObject = CreateSpriteGameObject(imagePath, new Vector2(indexX, indexY));
+        spriteRenderStatusDict[tileId] = SpriteRenderStatus.Rendered;
+        return spriteObject;
     }
 }
